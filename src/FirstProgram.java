6f9ed690fdfaf7299d95f9dc29dcc2d70d0458e6
Tetiana.Brukva <efadragon@gmail.com>
2018-09-10 19:18:48 +0300

    initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        String out = "Hello world!!!";
+        int i = 1;
+        int e = 22;
+
+        System.out.println(out + i +e);
+}
+}
